dd156436128052abb4de233c852f658226b8eb0e
yeasheng <yeasheng@126.com>
2020-03-29 15:44:42 +0800

    自动生成数据库表对应model和mapper

--- ymall-mbg/src/main/java/com/yeasheng/ymall/common/ymall/Generator.java
+++
@@ -1,8 +0,0 @@
-package com.yeasheng.ymall.common.ymall;
-
-public class Generator {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ ymall-mbg/src/main/java/com/yeasheng/ymall/mapper/UmsPermissionMapper.java
@@ -0,0 +1,30 @@
+package com.yeasheng.ymall.mapper;
+
+import com.yeasheng.ymall.model.UmsPermission;
+import com.yeasheng.ymall.model.UmsPermissionExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsPermissionMapper {
+    long countByExample(UmsPermissionExample example);
+
+    int deleteByExample(UmsPermissionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsPermission record);
+
+    int insertSelective(UmsPermission record);
+
+    List<UmsPermission> selectByExample(UmsPermissionExample example);
+
+    UmsPermission selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByExample(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByPrimaryKeySelective(UmsPermission record);
+
+    int updateByPrimaryKey(UmsPermission record);
+}
